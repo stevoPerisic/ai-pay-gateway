95e25f329e4e07c9a1f8cc587fe1a6ad28d3cf15
Stevo Perisic <stevo.perisic@gmail.com>
2025-08-11 21:32:09 -0400

    Update index.ts
    Add payment routes

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import { tasksRouter } from "./endpoints/tasks/router";
 import { ContentfulStatusCode } from "hono/utils/http-status";
 import { DummyEndpoint } from "./endpoints/dummyEndpoint";
+import { paymentRouter } from "./endpoints/payment/router";
 
 // Start a Hono app
 const app = new Hono<{ Bindings: Env }>();
@@ -45,6 +46,9 @@ openapi.route("/tasks", tasksRouter);
 
 // Register other endpoints
 openapi.post("/dummy/:slug", DummyEndpoint);
+
+// Register Payment Sub router
+openapi.route("/payment", paymentRouter);
 
 // Export the Hono app
 export default app;
